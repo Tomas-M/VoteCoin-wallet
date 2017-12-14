5602b96d5d277a711f8fe3e08955aa8e5c99dd36
Tomas M <tomas@slax.org>
2017-12-14 18:31:47 +0100

    allow only one instance

--- resources/app/main.js
+++ resources/app/main.js
@@ -45,6 +45,16 @@ function createWindow () {
   })
 }
 
+
+var shouldQuit = app.makeSingleInstance(function(commandLine, workingDirectory)
+{
+   // Someone tried to run a second instance, we should focus our window.
+   if (win) { if (win.isMinimized()) win.restore(); win.focus(); }
+});
+
+if (shouldQuit) { app.quit(); return; }
+
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
