6b412411ed79707866ef028aa1bdab75a0e38a7c
Tomas M <tomas@slax.org>
2018-01-08 17:02:40 +0100

    Add rcedit and script to simplify next electron->votecoin update

---
+++ rcedit/votecoin.bat
@@ -0,0 +1,7 @@
+rename electron.exe votecoin.exe
+rcedit-x64.exe votecoin.exe --set-version-string "CompanyName" "VoteCoin"
+rcedit-x64.exe votecoin.exe --set-version-string "FileDescription" "VoteCoin Wallet"
+rcedit-x64.exe votecoin.exe --set-version-string "LegalCopyright" "(C) VoteCoin Team 2018"
+rcedit-x64.exe votecoin.exe --set-version-string "ProductName" "VoteCoin Wallet"
+rcedit-x64.exe votecoin.exe --set-icon votecoin.ico
+pause
