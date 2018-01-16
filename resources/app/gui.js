fbc4e58e31000923ea44692a2348a3f01b638734
Tomas M <tomas@slax.org>
2018-01-16 13:26:05 +0100

    explicit sync 0%

--- resources/app/gui.js
+++ resources/app/gui.js
@@ -22,7 +22,7 @@ function update_gui()
     settext('blockcurrent',num(blocks));
     settext('blocktotal',num(totalblocks>blocks?totalblocks:blocks));
 
-    if (connections==0 || blocks==0 || totalblocks==0) settext('blockcurrent','sync',true);
+    if (connections==0 || blocks==0 || totalblocks==0) settext('blockcurrent','sync 0%',true);
     else if (totalblocks-2>blocks) settext('blockcurrent','sync '+Math.floor(blocks/totalblocks*100)+'%',true);
     else settext('blockcurrent','sync ok');
 
