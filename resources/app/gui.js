874e367c3a4b197ae858e21de83a51ac143c838e
Tomas M <tomas@slax.org>
2018-02-01 10:45:25 +0100

    limit sound once per 5 seconds

--- resources/app/gui.js
+++ resources/app/gui.js
@@ -81,7 +81,11 @@ function update_gui()
         for (i=0; i<transactions.length; i++) max=transactions[i].amount>max?transactions[i].amount:max;
         for (i=0; i<transactions.length; i++) chart+='<div title="'+humanReadableDate(transactions[i].blocktime||transactions[i].time)+'\n'+UCfirst(transactions[i].category)+" "+num(Math.abs(transactions[i].amount,8))+' VOT" class="chartbar '+(transactions[i].amount<0?'minus':'')+'" style="width: calc('+100/transactions.length+'% - 2px); height: '+Math.ceil(Math.abs(transactions[i].amount)/max*150)+'px"></div>';
         sethtml('transactionchart',chart);
-        if (!listWasEmpty) $('#sound').html('<audio autoplay="autoplay"><source src="notify.mp3" type="audio/mpeg" /></audio>');
+        if (!listWasEmpty && $('#sound').html()=='')
+        {
+           $('#sound').html('<audio autoplay="autoplay"><source src="notify.mp3" type="audio/mpeg" /></audio>');
+           setTimeout(function(){ $('#sound').html(''); },5000); // prevents replaying the notification in the next 5 seconds
+        }
     }
 
     var from=$('#choosefrom').val();
