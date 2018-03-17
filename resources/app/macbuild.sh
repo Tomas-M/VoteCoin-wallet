b421f9184aee099ff3d0e16fdc5d7810262c5516
Tomas M <tomas@slax.org>
2018-03-17 08:10:18 +0100

    build script for mac & linux

--- resources/app/macbuild.sh
+++ resources/app/macbuild.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 npm install .
-electron-packager . --icon=votecoin.icns
+electron-packager . --platform=darwin,linux --icon=votecoin.icns
+rm "VoteCoin Wallet-darwin-x64/VoteCoin Wallet.app/Contents/Resources/app/votecoind.exe"
+rm "VoteCoin Wallet-darwin-x64/VoteCoin Wallet.app/Contents/Resources/app/votecoind.ux"
+rm "VoteCoin Wallet-linux-x64/resources/app/votecoind.exe"
+rm "VoteCoin Wallet-linux-x64/resources/app/votecoind.mac"
