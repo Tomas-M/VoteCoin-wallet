75cd3f17968018500b9aa65edfcd9154906b406e
Tomas M <tomas@slax.org>
2018-03-17 08:55:49 +0100

    make sure binaries are executable

--- resources/app/macbuild.sh
+++ resources/app/macbuild.sh
@@ -3,5 +3,7 @@ npm install .
 electron-packager . --platform=darwin,linux --icon=votecoin.icns
 rm "VoteCoin Wallet-darwin-x64/VoteCoin Wallet.app/Contents/Resources/app/votecoind.exe"
 rm "VoteCoin Wallet-darwin-x64/VoteCoin Wallet.app/Contents/Resources/app/votecoind.ux"
+chmod ugo+x "VoteCoin Wallet-darwin-x64/VoteCoin Wallet.app/Contents/Resources/app/votecoind.mac"
 rm "VoteCoin Wallet-linux-x64/resources/app/votecoind.exe"
 rm "VoteCoin Wallet-linux-x64/resources/app/votecoind.mac"
+chmod ugo+x "VoteCoin Wallet-linux-x64/resources/app/votecoind.ux"
