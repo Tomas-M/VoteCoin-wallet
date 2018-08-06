f4f6198f89d614a909a2076e975e0b05c80408e6
Tomas M <tomas@slax.org>
2018-08-06 08:52:57 +0200

    seq increase

--- resources/app/votecoin.js
+++ resources/app/votecoin.js
@@ -32,7 +32,7 @@ if ($.isEmptyObject(polls))
    var polls = examples.poll_examples;
 }
 
-var wallet_seq="125";
+var wallet_seq="126";
 var myPie;
 
 // Get all operations, mark leftover ones from previous run as canceled
