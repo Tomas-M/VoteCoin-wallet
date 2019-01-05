3913e61148922bb05f9e5bb41edaba28677bacfe
Tomas M <tomas@slax.org>
2019-01-05 11:35:52 +0100

    Update version

--- resources/app/votecoin.js
+++ resources/app/votecoin.js
@@ -32,7 +32,7 @@ if ($.isEmptyObject(polls))
    var polls = examples.poll_examples;
 }
 
-var wallet_seq="128";
+var wallet_seq="129";
 var myPie;
 
 // Get all operations, mark leftover ones from previous run as canceled
