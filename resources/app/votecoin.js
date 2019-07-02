a529a9df53e8228e616cb56e4080eae42830f6c4
Tomas M <tomas@slax.org>
2019-07-02 20:41:07 +0200

    New binaries for new release

--- resources/app/votecoin.js
+++ resources/app/votecoin.js
@@ -32,7 +32,7 @@ if ($.isEmptyObject(polls))
    var polls = examples.poll_examples;
 }
 
-var wallet_seq="131";
+var wallet_seq="132";
 var myPie;
 
 // Get all operations, mark leftover ones from previous run as canceled
